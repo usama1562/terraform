ad64d33f8c2500dbed1bbfefea2083035d09b449
usama1562 <usamaishaikh41@gmail.com>
2024-05-28 19:18:20 +0530

    Update instance.tf
    sss

--- instance.tf
+++ instance.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "instance" {
   ami                    = "ami-0bb84b8ffd87024d8"
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-09f186e51cf491527", "sg-0dfe9ecea3b03dca3"]
-  key_name               = "1"
+  vpc_security_group_ids = ["sg-00eb56845dea9da55", "sg-0cd1acafcca4103b2"]
+  key_name               = "tf"
 }
 
