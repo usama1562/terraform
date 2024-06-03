d8fe2ce57a9008d81069921509b0fbd347108b89
usama1562 <usamaishaikh41@gmail.com>
2024-06-03 17:38:57 +0530

    Add files via upload

--- instance.tf
+++ instance.tf
@@ -2,63 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "usa_tls" {
-  name        = "usa_tls"
-  description = "Allow SSH for all network"
-  vpc_id      = "vpc-0b108c47b1923e4ae" # VPC ID should be a string
-
-  ingress {
-    description      = "SSH rule"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+resource "aws_instance" "app_server" {
+  ami           = "ami-00beae93a2d981137"  # Replace with a valid AMI ID
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "usa_tls"
-    Env  = "Dev"
+    Name = "AppServer"
   }
-}
-
-data "aws_ami" "my_ami" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical (Ubuntu) account ID
-
-  filter {
-    name   = "name"
-    values = ["ubuntu-pro-server/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-pro-server-20240423"]
-  }
-}
-
-resource "aws_instance" "instance2" {
-  ami                    = data.aws_ami.my_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.usa_tls.id]
-  key_name               = "l1"
-
-  tags = {
-    Name = "usa_tls"
-    Env  = "Dev"
-  }
-}
-
-variable "instance_type" {
-  type    = string
-  default = "t4g.micro" # Changed to an arm64 compatible instance type
-}
-
-output "hellow" {
-  value = "hello world" # Correct spelling
-}
-
-output "public_ip" {
-  value = aws_instance.instance2.public_ip
 }
--- main.tf
+++ main.tf
@@ -1,38 +1,65 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_instance" "this_aws_instance" {
+  ami                    = var.this_aws_instance_ami
+  instance_type          = var.this_aws_instance_instance_type
+  key_name               = var.this_aws_instance_key_name
+  vpc_security_group_ids =  [aws_security_group.this_sg.id]
+//[var.this_aws_instance_sg_id]
+  availability_zone      = var.this_aws_instance_availability_zone
+  subnet_id = var.this_aws_instance_subnet 
+  root_block_device {
+    volume_size = var.this_aws_instance_volume_size
+  }
+  count = var.this_aws_instance_count
+  //user_data_base64 = var.this_aws_instance_user_data_base64
+    user_data = <<-EOF
+            #!/bin/bash
+    sudo -i
+    
+    EOF 
+
+
+
+  tags = {
+      
+      Name =  var.this_aws_instance_tags
+  }
 }
 
-module "vpc" {
-    source = "./module/vpc"
-    this_vpc_cidr_block = "12.11.0.0/16"
-    this_vpc_tags = "this_vpc"
-    this_subnet_pub_cidr_block = "12.11.0.0/17"
-    this_subnet_private1_cidr_block =  "12.11.128.0/19"
-    this_subnet_pub_map_ip = true
-    this_subnet_private_map_ip = false
-    this_subnet_pub_tags  = "pub_subnet"
-    this_subnet_private1_tags = "pri_subnet1"
-    this_subnet_private2_cidr_block = "12.11.192.0/20"
-    this_subnet_private2_tags = "pri_subnet2"
-    this_igw_tags = "igw"
-    this_def_Route_example_cidr_block = "0.0.0.0/0"
-    this_def_Route_example_tag = "default" 
-} 
+resource "aws_security_group" "this_sg" {
+  vpc_id = var.this_sg_vpc_id
 
-/*
-module "ec2" {
-    source = "./module/ec2"
-    this_aws_instance_ami = "ami-0eb5115914ccc4bc2"
-    this_aws_instance_instance_type  = "t2.micro"
-    //this_aws_instance_subnet = module.aws_vpc.aws_subnet_this_public
-    this_aws_instance_key_name = "delete_oregon_anup"
-    this_aws_instance_availability_zone = "us-west-2b"
-    this_aws_instance_volume_size = 15
-    this_aws_instance_count = 2
-    this_aws_instance_user_data_base64 = true
-    this_aws_instance_tags =  "Terraform_instance"
-    this_aws_instance_subnet = module.vpc.subnet_id
-    this_sg_vpc_id = module.vpc.vpc_id
-    //this_aws_vpc_id = module.aws_vpc.aws_vpc_id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+
 }
-*/
